80942f4b2c49646d24f563c5a9faf516e8b1af35
zenfusdesign <mariusduponcel@gmail.com>
2021-08-30 14:20:15 +0200

    alez

--- public/script2.js
+++ public/script2.js
@@ -175,7 +175,6 @@ function CartOpen () {
 
 async function getProducts() {
     const response  = await axios.get('https://roger-roger-b38ef.ondigitalocean.app/products')
-    //const response  = await axios.get('http://localhost:5000/products')
     console.log(response.data)
     products = response.data.products
 
